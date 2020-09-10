e5aa251e2f1a3d3ac23d759a639b3fd9c7dacc95
kinghwj <13040285168@163.com>
2020-09-10 18:21:28 +0800

    更新

--- Java/Code/高级/day_05/src/ThreadSecurity/Test.java
+++ Java/Code/高级/day_05/src/ThreadSecurity/Test.java
@@ -2,10 +2,12 @@ package ThreadSecurity;
 
 public class Test {
     public static void main(String[] args) {
+        int num3 = 01;
+        System.out.println(num3);
         System.out.println(new A().a);
     }
 }
 
-class A{
+class A {
     String a;
 }
