6804d0e9edc5e188bcec5306e27262c99310ca48
kinghwj <13040285168@163.com>
2020-11-20 15:57:03 +0800

    数组结构，增删改查

--- Java/Code/AlgorithmsAndDataStructures/src/DataStructures/Array/MyArray.java
+++ Java/Code/AlgorithmsAndDataStructures/src/DataStructures/Array/MyArray.java
@@ -10,32 +10,115 @@ public class MyArray {
 
     public MyArray() {
         elems = 0;
-        length = 50;
+        length = 5;
         intArray = new int[length];
     }
-
-    /*
-    　 ①、如何插入一条新的数据项
-
-    　　②、如何寻找某一特定的数据项
-
-    　　③、如何删除某一特定的数据项
-
-    　　④、如何迭代的访问各个数据项，以便进行显示或其他操作
-     */
-
+    
     // 获取数组有效长度
     public int getSize(){
         return elems;
     }
-
-    public void add(int num){
-
+    
+    // 显示数组元素
+    public void display(){
+        for (int i : intArray) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
+    // 增：插入新的数据
+    public boolean add(int num){
+        if(elems > length){
+            System.out.println("超出数组最大长度");
+            return false;
+        }else{
+            intArray[elems] = num;
+            elems++;
+            return true;
+        }
+    }
+
+    // 删：删除元素
+    public boolean delete(int num){
+        if(!contain(num)){
+            System.out.println("删除失败，该数据不在数组中");
+           return false;
+        }else{
+            int i;
+            for(i = find(num);i<elems-1;i++){
+                intArray[i] = intArray[i+1];
+            }
+            intArray[elems-1] = 0;
+            elems--;
+        }
+        return true;
+    }
+
+    // 改：修改元素
+    public boolean update(int oldValue,int newValue){
+        int i = find(oldValue);
+        if(i == -1){
+            System.out.println("修改失败，该数据不在数组中");
+            return false;
+        }
+        intArray[i] = newValue;
+        return true;
+    }
+
+    // 查：查找某个数据项是否在数组中
+    public boolean contain(int num){
+        for (int i : intArray) {
+            if(i == num){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查：查找某个数据项第一次出现的索引
+    public int find(int num){
+        int i;
+        for (i = 0; i < elems; i++) {
+            if(intArray[i] == num){
+                break;
+            }
+        }
+        if(i == elems){
+            return -1;
+        }
+        return i;
+    }
+
+    // 查：根据下标获取到具体的值
+    public int get(int i){
+        if(i < 0 || i >= elems){
+            System.out.println("超出最大索引范围");
+        }
+        return intArray[i];
+    }
 
     public static void main(String[] args) {
         MyArray oneArray = new MyArray();
+        oneArray.add(10);
+        oneArray.add(10);
+        oneArray.add(3);
+        oneArray.add(1);
+        oneArray.add(7);
+        System.out.println(oneArray.get(1));
+        oneArray.display();
+        System.out.println(oneArray.contain(3));
+        System.out.println(oneArray.delete(7));
+        oneArray.display();
         System.out.println(oneArray.getSize());
+        System.out.println(oneArray.update(10,20));
+        oneArray.display();
+//
+//        oneArray.add(20);
+//        System.out.println(oneArray.getSize());
+//        System.out.println(oneArray.contain(10));
+//        System.out.println(oneArray.contain(1));
+//        System.out.println(oneArray.find(1));
+//        System.out.println(oneArray.find(20));
     }
 }
